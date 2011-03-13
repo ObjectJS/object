82cdf04c9d00871eac984bb1c8f46bdaa2236f86
jingwei.li@XIAONEI.OPI.COM@b39729cf-7639-446e-ac28-3c6a20f25fe3 <jingwei.li@XIAONEI.OPI.COM@b39729cf-7639-446e-ac28-3c6a20f25fe3>
2011-03-13 16:01:06 +0000

    Array.prototype.reduce & Array.prototype.reduceRight
    --HG--
    extra : convert_revision : svn%3Ab39729cf-7639-446e-ac28-3c6a20f25fe3/static/object%4018440

--- trunk/src/object.js
+++ trunk/src/object.js
@@ -82,6 +82,92 @@ Array.prototype.filter = Array.prototype.filter || function(fn, bind){
 	return results;
 };
 
+Array.prototype.reduce = Array.prototype.reduce || function(fun /*, initialValue */) {
+	"use strict";
+
+	if (this === void 0 || this === null)
+		throw new TypeError();
+
+	var t = Object(this);
+	var len = t.length >>> 0;
+	if (typeof fun !== "function")
+		throw new TypeError();
+
+	// no value to return if no initial value and an empty array
+	if (len == 0 && arguments.length == 1)
+		throw new TypeError();
+
+	var k = 0;
+	var accumulator;
+	if (arguments.length >= 2) {
+		accumulator = arguments[1];
+	} else {
+		do {
+			if (k in t) {
+				accumulator = t[k++];
+				break;
+			}
+
+			// if array contains no values, no initial value to return
+			if (++k >= len) {
+				throw new TypeError();
+			}
+
+		} while (true);
+	}
+
+	while (k < len) {
+		if (k in t)
+			accumulator = fun.call(undefined, accumulator, t[k], k, t);
+		k++;
+	}
+
+	return accumulator;
+};
+
+Array.prototype.reduceRight = Array.prototype.reduceRight || function(callbackfn /*, initialValue */) {
+	"use strict";
+
+	if (this === void 0 || this === null)
+		throw new TypeError();
+
+	var t = Object(this);
+	var len = t.length >>> 0;
+	if (typeof callbackfn !== "function")
+		throw new TypeError();
+
+	// no value to return if no initial value, empty array
+	if (len === 0 && arguments.length === 1)
+		throw new TypeError();
+
+	var k = len - 1;
+	var accumulator;
+	if (arguments.length >= 2) {
+		accumulator = arguments[1];
+	} else {
+		do {
+			if (k in this) {
+				accumulator = this[k--];
+				break;
+			}
+
+			// if array contains no values, no initial value to return
+			if (--k < 0) {
+				throw new TypeError();
+			}
+		}
+		while (true);
+	}
+
+	while (k >= 0) {
+		if (k in t)
+			accumulator = callbackfn.call(undefined, accumulator, t[k], k, t);
+		k--;
+	}
+
+	return accumulator;
+};
+
 Function.prototype.bind = Function.prototype.bind || function(object) {
 	var method = this;
 	return function() {
