cda80d7069786d7cc4a9163941cfe65be921fc4a
jingwei.li@XIAONEI.OPI.COM@b39729cf-7639-446e-ac28-3c6a20f25fe3 <jingwei.li@XIAONEI.OPI.COM@b39729cf-7639-446e-ac28-3c6a20f25fe3>
2011-03-13 09:16:41 +0000

    comment + format
    --HG--
    extra : convert_revision : svn%3Ab39729cf-7639-446e-ac28-3c6a20f25fe3/static/object%4018434

--- trunk/src/ui/decorators.js
+++ trunk/src/ui/decorators.js
@@ -2,34 +2,36 @@
  * @namespace
  * @name ui.decorators
  */
-object.add('ui.decorators', /**@lends ui.decorators*/ function(exports, string, dom) {
+object.add('ui.decorators', /**@lends ui.decorators*/ function(exports) {
 
-	/**
-	 * fireevent 或 fireevent(eventName)
-	 * fireevent 默认eventName通过__name__获得
-	 */
-	this.fireevent = function(func) {
-		// 自定义了事件名称，返回一个decorator
-		if (typeof func == 'string') {
-			var name = func;
-			return function(func) {
-				var result = function(self) {
-					var handleName = 'on' + name;
-					if (self[handleName]) self[handleName]();
-					self._node.fireEvent(name, null, self);
-					func.apply(this, arguments);
-				};
-				return result;
-			};
-		} else {
-			return function(self) {
-				var name = arguments.callee.__name__;
+/**
+ * decorator
+ * 使得相应方法在调用时fire出同名事件，并调用相应的 onxxx 方法
+ * fireevent 或 fireevent(eventName)
+ * fireevent 默认eventName通过__name__获得
+ */
+this.fireevent = function(func) {
+	// 自定义了事件名称，返回一个decorator
+	if (typeof func == 'string') {
+		var name = func;
+		return function(func) {
+			var result = function(self) {
 				var handleName = 'on' + name;
 				if (self[handleName]) self[handleName]();
 				self._node.fireEvent(name, null, self);
 				func.apply(this, arguments);
 			};
-		}
-	};
+			return result;
+		};
+	} else {
+		return function(self) {
+			var name = arguments.callee.__name__;
+			var handleName = 'on' + name;
+			if (self[handleName]) self[handleName]();
+			self._node.fireEvent(name, null, self);
+			func.apply(this, arguments);
+		};
+	}
+};
 
 });
