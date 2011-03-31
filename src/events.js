0a6897ce8944edce3a7eab93ddf94905bd581970
jingwei.li@XIAONEI.OPI.COM@b39729cf-7639-446e-ac28-3c6a20f25fe3 <jingwei.li@XIAONEI.OPI.COM@b39729cf-7639-446e-ac28-3c6a20f25fe3>
2011-03-31 03:23:55 +0000

    将fireevent方法放到events模块下，删除dom.event模块
    --HG--
    extra : convert_revision : svn%3Ab39729cf-7639-446e-ac28-3c6a20f25fe3/static/object%4019167

--- src/events.js
+++ src/events.js
@@ -4,6 +4,41 @@
  */
 object.add('events', /**@lends events*/ function(exports) {
 
+/**
+ * decorator
+ * 使得相应方法在调用时fire出同名事件，并调用相应的 onxxx 方法
+ * fireevent 或 fireevent(eventName)
+ * fireevent 默认eventName通过__name__获得
+ */
+this.fireevent = function(funcOrName) {
+	var name, func;
+
+	// 千万别给这个function起名字，否则fire出来的事件都叫一个名字
+	var firer = function(self) {
+		if (!name) name = arguments.callee.__name__;
+
+		var eventData = {};
+		var handleName = 'on' + name;
+		if (self[handleName]) self[handleName]();
+		var event = self.fireEvent(name, eventData, self);
+		var preventDefaulted = event.getPreventDefault? event.getPreventDefault() : event.defaultPrevented;
+		if (!preventDefaulted) func.apply(this, arguments);
+		return event;
+	}
+
+	// 自定义了事件名称，返回一个decorator
+	if (typeof funcOrName == 'string') {
+		name = funcOrName;
+		return function(_func) {
+			func = _func;
+			return firer;
+		};
+	} else {
+		func = funcOrName;
+		return firer;
+	}
+};
+
 // 事件
 this.Events = new Class(/**@lends object.Events*/ {
 
--- src/ui/decorators.js
+++ src/ui/decorators.js
@@ -2,41 +2,11 @@
  * @namespace
  * @name ui.decorators
  */
-object.add('ui.decorators', /**@lends ui.decorators*/ function(exports) {
+object.add('ui.decorators', 'events', /**@lends ui.decorators*/ function(exports, events) {
 
-/**
- * decorator
- * 使得相应方法在调用时fire出同名事件，并调用相应的 onxxx 方法
- * fireevent 或 fireevent(eventName)
- * fireevent 默认eventName通过__name__获得
- */
-this.fireevent = function(funcOrName) {
-	var name, func;
-
-	// 千万别给这个function起名字，否则fire出来的事件都叫一个名字
-	var firer = function(self) {
-		if (!name) name = arguments.callee.__name__;
-
-		var eventData = {};
-		var handleName = 'on' + name;
-		if (self[handleName]) self[handleName]();
-		var event = self._node.fireEvent(name, eventData, self);
-		var preventDefaulted = event.getPreventDefault? event.getPreventDefault() : event.defaultPrevented;
-		if (!preventDefaulted) func.apply(this, arguments);
-		return event;
-	}
-
-	// 自定义了事件名称，返回一个decorator
-	if (typeof funcOrName == 'string') {
-		name = funcOrName;
-		return function(_func) {
-			func = _func;
-			return firer;
-		};
-	} else {
-		func = funcOrName;
-		return firer;
-	}
-};
+	/**
+	 * use events.fireevent instead
+	 */
+	this.fireevent = events.fireevent;
 
 });
