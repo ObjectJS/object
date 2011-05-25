8ded7670b56b2bad6f4489da5916170296bda768
jingwei.li <none@none>
2011-05-25 21:25:06 +0800

    test

--- doc.bat
+++ doc.bat
@@ -1,3 +1,4 @@
+
 REM rmdir /S /Q d:\works\renren\static\object\trunk\doc
 set JSDOC=d:/works/jsdoc-toolkit
 java -jar %JSDOC%/jsrun.jar %JSDOC%/app/run.js -a -r=10 -t=%JSDOC%/templates/codeview -d=d:/works/renren/static/object/trunk/doc/ d:/works/renren/static/object/trunk/src/
