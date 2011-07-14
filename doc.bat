f4a3b9de5d8ff43aaa07bf1fe01bca88b533f614
jingwei.li <none@none>
2011-07-14 18:36:13 +0800

    doc
    --HG--
    rename : doc/symbols/src/d__works_renren_static_object_trunk_src__setGlobal.js.html => doc/symbols/src/d__works_workspace_objectjs.org_object_src__setGlobal.js.html
    rename : doc/symbols/src/d__works_renren_static_object_trunk_src__urlparse.js.html => doc/symbols/src/d__works_workspace_objectjs.org_object_src__urlparse.js.html
    rename : doc/symbols/src/d__works_renren_static_object_trunk_src__validator.js.html => doc/symbols/src/d__works_workspace_objectjs.org_object_src__validator.js.html

--- doc.bat
+++ doc.bat
@@ -1,3 +1,3 @@
 REM rmdir /S /Q d:\works\renren\static\object\trunk\doc
 set JSDOC=d:/works/jsdoc-toolkit
-java -jar %JSDOC%/jsrun.jar %JSDOC%/app/run.js -a -r=10 -t=%JSDOC%/templates/codeview -d=d:/works/renren/static/object/trunk/doc/ d:/works/renren/static/object/trunk/src/
+java -jar %JSDOC%/jsrun.jar %JSDOC%/app/run.js -a -r=10 -t=%JSDOC%/templates/codeview -d=d:/works/workspace/objectjs.org/object/doc/ d:/works/workspace/objectjs.org/object/src/
