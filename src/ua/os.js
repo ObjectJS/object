b3bf84dd463a828db0b2b900615adea389c2584e
zhifu.wang <none@none>
2011-11-02 16:41:56 +0800

    modify os.js for IE
    --HG--
    branch : feature/unit-test

--- src/ua/os.js
+++ src/ua/os.js
@@ -216,8 +216,12 @@ function detectOS(ua) {
 	//检测屏幕方向，首先确保支持屏幕方向
 	var isMobile = typeof window.orientation != 'undefined' ? true : false;
 	if(isMobile) {
-		//通过屏幕的高度和宽度的值大小，来判断是横向还是纵向
-		o.orientation = window.innerWidth > window.innerHeight ? 'profile' : 'landscape';
+		if(window.innerWidth != undefined) {
+			//通过屏幕的高度和宽度的值大小，来判断是横向还是纵向
+			o.orientation = window.innerWidth > window.innerHeight ? 'profile' : 'landscape';
+		} else {
+			o.orientation = window.screen.availWidth > window.screen.availHeight ? 'profile' : 'landscape';
+		}
 	} else {
 		o.orientation = 'unknown';
 	}
