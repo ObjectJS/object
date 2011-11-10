02cdecdab31c30e0792f3fd9aaf516cc7c851c36
zhifu.wang <none@none>
2011-11-10 18:10:38 +0800

    add testcases for events.js
    --HG--
    branch : feature/unit-test

--- test/unit/modules/events/index.js
+++ test/unit/modules/events/index.js
@@ -2,5 +2,6 @@ $(document).ready(function() {
 	var path = $LAB.needPath ? 'modules/events/' : '';
 	$LAB
 	   .script(path + "../../../../src/events.js").wait()
-	   .script(path + "events-basic.js").wait()
+	   //.script(path + "events-basic.js").wait()
+	   .script(path + "events-usage.js").wait()
 });
