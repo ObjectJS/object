402b2b9b8a446285d07484505dc71ff2f712f5bb
zhifu.wang <none@none>
2011-11-10 15:12:53 +0800

    add testcase for events.js
    --HG--
    branch : feature/unit-test

---
+++ test/unit/modules/events/events-basic.js
@@ -0,0 +1,5 @@
+module('events');
+
+test('events usable', function() {
+
+});
--- test/unit/modules/events/index.js
+++ test/unit/modules/events/index.js
@@ -1,2 +1,6 @@
 $(document).ready(function() {
+	var path = $LAB.needPath ? 'modules/events/' : '';
+	$LAB
+	   .script(path + "../../../../src/events.js").wait()
+	   .script(path + "events-basic.js").wait()
 });
