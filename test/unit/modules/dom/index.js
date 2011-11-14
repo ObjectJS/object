2f4cdd560152c204388d3107d95f15297e7548f2
zhifu.wang <none@none>
2011-11-14 16:23:07 +0800

    add dom.ready testcases by iframe(not supported in Chrome 5+)
    --HG--
    branch : feature/unit-test

--- test/unit/modules/dom/index.js
+++ test/unit/modules/dom/index.js
@@ -2,8 +2,7 @@ $(document).ready(function() {
 	var path = $LAB.needPath ? 'modules/dom/' : '';
 	$LAB
 	   .script(path + "../../../../src/dom/index.js").wait()
-	   //.script(path + "dom-basic.js").wait()
-	   //.script(path + "dom-ready.js").wait()
-	   .script(path + "dom-ready-iframe-in-chrome.js").wait()
-	   //.script(path + "dom-usage.js").wait()
+	   .script(path + "dom-basic.js").wait()
+	   .script(path + "dom-ready.js").wait()
+	   .script(path + "dom-usage.js").wait()
 });
