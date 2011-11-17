0a64de00979236dd61ff6238b488018ec2b4cfdb
zhifu.wang <none@none>
2011-11-17 18:48:42 +0800

    remove repeated method in events.js, add testcases
    --HG--
    branch : feature/unit-test

--- test/unit/modules/events/index.js
+++ test/unit/modules/events/index.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 	var path = $LAB.needPath ? 'modules/events/' : '';
 	$LAB
 	   .script(path + "../../../../src/events.js").wait()
-	   .script(path + "events-basic.js").wait()
-	   .script(path + "events-usage.js").wait()
+	   //.script(path + "events-basic.js").wait()
+	   //.script(path + "events-usage.js").wait()
+	   .script(path + "events-onxxx.js").wait()
 });
