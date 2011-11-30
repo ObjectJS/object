cb5140250fbdc532deab07ffe27161e7d1f01bbe
zhifu.wang <none@none>
2011-11-30 21:11:01 +0800

    remove wrong name files
    --HG--
    branch : feature/unit-test

--- test/unit/module/index.js
+++ test/unit/module/index.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-	var path = $UNIT_TEST_CONFIG.needPath ? 'module/' : '';
-	$LAB.script(path + 'module-usage.js');
+     var path = $UNIT_TEST_CONFIG.needPath ? 'module/' : '';
+     $LAB.script(path + 'module-usage.js');
 });
