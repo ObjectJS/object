370f95e17e56a48ebf601ff5cc8b1cdb7fd1816e
jingwei.li <none@none>
2012-03-09 01:41:16 +0800

    修复urljoin的问题
    --HG--
    branch : feature/urlparse

--- src/urlparse.js
+++ src/urlparse.js
@@ -1,74 +1,45 @@
-object.add('urlparse', function() {
+object.add('urlparse', function(exports) {
 
 /**
-* 合并两段url
-*/
-this.urljoin = function(base, url) {
-	if (!base || !url || typeof base != 'string' || typeof url != 'string') {
-		return '';
-	}
-	var baseparts = urlparse(base);
-	var urlparts = urlparse(url);
-	var output = [];
-
-	if (urlparts[0]) {
-		return url;
-	} else {
-		output[0] = baseparts[0];
-		output[1] = baseparts[1];
-	}
-
-	if (urlparts[2]) {
-		// 判断第一个字符，在IE6下不能用urlparts[2][0]的方式，而需要采用charAt
-		if (urlparts[2].charAt(0) == '/') {
-			output[2] = urlparts[2];
-		} else {
-			path = baseparts[2];
-			if (path) {
-				output[2] = path.substring(0, path.lastIndexOf('/') + 1) + urlparts[2];
-			} else {
-				output[2] = '/' + urlparts[2];
-			}
-		}
-	} else {
-		return base;
-	}
-
-	return urlunparse(output);
-};
-
-/**
-* 解析一个url为 scheme / netloc / path / params / query / fragment 六个部分
-* @see http://docs.python.org/library/urlparse.html
-*/
-var urlparse = this.urlparse = function(url) {
-	if (!url || typeof url != 'string') {
+ * 解析一个url为 scheme / netloc / path / params / query / fragment 六个部分
+ * @see http://docs.python.org/library/urlparse.html
+ */
+function urlparse(url, scheme) {
+	var reg, parts;
+	if (typeof url != 'string') {
 		return null;
 	}
 	url = url.trim();
 	
 	if (url.indexOf('file') == 0) {
 		// file:///F:/works/workspace/objectjs.org/object/test/unit/modules/urlparse/index.html
-		var reg = /^(file)\:\/\/()([^\?]*?)?(?:;(.*?))?(?:\?(.*?))?(?:\#(.*))?$/i
+		reg = /^(file)\:\/\/()([^\?]*?)?(?:;(.*?))?(?:\?(.*?))?(?:\#(.*))?$/i
 	} else {
 		// http://www.renren.com:8080/home;32131?id=31321321&a=1#//music/?from=homeleft#fdalfdjal
-		var reg = /^(?:(\w+?)\:\/(?:\/)?([\w-_.]+(?::\w+)?))?([^\?]*?)?(?:;(.*?))?(?:\?(.*?))?(?:\#(.*))?$/i;
+		reg = /^(?:(\w+?)\:\/(?:\/)?([\w-_.]+(?::\w+)?))?([^\?]*?)?(?:;(.*?))?(?:\?(.*?))?(?:\#(.*))?$/i;
 	}
 	if (reg.test(url)) {
-		return url.match(reg).slice(1);
+		parts = url.match(reg).slice(1);
+		if (!parts[0] && scheme) parts[0] = scheme;
+		for (var i = 0; i < parts.length; i++) {
+			if (!parts[i]) parts[i] = '';
+		}
+		return parts;
+	} else {
+		return ['', '', '', '', '', ''];
 	}
-	
 };
 
 /**
 * 将兼容urlparse结果的url部分合并成url
 */
-var urlunparse = this.urlunparse = function(parts) {
+function urlunparse(parts) {
 	if (!parts) {
 		return '';
 	}
 	var url = '';
 	if (parts[0]) url += parts[0] + '://' + parts[1];
+	if (parts[1] && parts[2] && parts[2].indexOf('/') != 0) url += '/';
 	url += parts[2];
 	if (parts[3]) url += ';' + parts[3];
 	if (parts[4]) url += '?' + parts[4];
@@ -76,6 +47,102 @@ var urlunparse = this.urlunparse = function(parts) {
 
 	return url;
 };
+
+/**
+* 合并两段url
+*/
+function urljoin(base, url) {
+
+	if (!base) {
+		return url;
+	}
+
+	if (!url) {
+		return base;
+	}
+
+	url = String(url);
+	base = String(base);
+
+	var bparts = urlparse(base);
+	var parts = urlparse(url, bparts[0]);
+
+	// scheme
+	if (parts[0] != bparts[0]) {
+		return url;
+	}
+
+	// netloc
+	if (parts[1]) {
+		return urlunparse(parts);
+	}
+
+	parts[1] = bparts[1];
+
+	// path
+	if (parts[2].charAt(0) == '/') {
+		return urlunparse(parts);
+	}
+
+	// params
+	if (!parts[2] && !parts[3]) {
+		parts[2] = bparts[2];
+		parts[3] = bparts[3];
+		if (!parts[4]) {
+			parts[4] = bparts[4];
+		}
+		return urlunparse(parts);
+	}
+
+    var segments = bparts[2].split('/').slice(0, -1).concat(parts[2].split('/'))
+
+	// 确保能够生成最后的斜线
+	if (segments[segments.length - 1] == '.') {
+		segments[segments.length - 1] = '';
+	}
+
+	// 去掉所有'.'当前目录
+	for (var i = 0, l = segments.length; i < l; i++) {
+		if (segments[i] == '.') {
+			segments.splice(i, 1);
+			i--;
+		}
+	}
+
+	// 合并所有'..'
+	var i;
+	while (true) {
+		i = 1;
+		n = segments.length - 1;
+		while (i < n) {
+			if (segments[i] == '..' && ['', '..'].indexOf(segments[i - 1]) == -1) {
+				segments.splice(i - 1, 2);
+				break;
+			}
+			i++;
+		}
+		if (i >= n) {
+			break;
+		}
+	}
+
+	if (segments.length == 2 && segments[0] == '' && segments[1] == '..') {
+		segments[segments.length - 1] = '';
+	}
+	else if (segments.length >= 2 && segments[segments.length - 1] == '..') {
+		segments.pop();
+		segments.pop();
+		segments.push('');
+	}
+
+	parts[2] = segments.join('/');
+
+	return urlunparse(parts);
+}
+
+exports.urlparse = urlparse;
+exports.urlunparse = urlunparse;
+exports.urljoin = urljoin;
 
 });
 
