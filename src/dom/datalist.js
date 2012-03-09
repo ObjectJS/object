39a8f6860651ea107e234b4f74c77179521eeb2a
zhifu.wang <none@none>
2012-03-09 19:14:43 +0800

    add comment
    --HG--
    branch : feature/datalist

--- src/dom/datalist.js
+++ src/dom/datalist.js
@@ -1,5 +1,7 @@
+// TODO 注意可能存在的属性名称冲突
 object.add('dom.datalist', 'dom, ua, sys', function(exports, dom, ua, sys) {
 
+	// 定义键位与名称的映射
 	var KEY = {
 		UP: 38,
 		DOWN: 40,
@@ -10,6 +12,7 @@ object.add('dom.datalist', 'dom, ua, sys', function(exports, dom, ua, sys) {
 		BACKSPACE: 8
 	};
 
+	// 一些默认设置（貌似不能通过传参来设置）
 	var defaultOptions = {
 		maxHeight: 190,
 		maxCharCount: 150,
@@ -17,16 +20,40 @@ object.add('dom.datalist', 'dom, ua, sys', function(exports, dom, ua, sys) {
 		matchFirst : false
 	}
 
+	/**
+	 * 数据列表实现类，模拟HTML5的input元素的list属性
+	 */
 	this.DataList = new Class(function() {
 
+		/**
+		 * 初始化方法，判断list属性是否存在，并调用init方法
+		 */
 		this.initialize = function(self) {
 			var datalistId = self.getAttribute('list');
-			if (datalistId == null || !document.getElementById(datalistId)) {
+			if (datalistId == null) {
 				return;
+			} else if(!document.getElementById(datalistId)) {
+				throw new Error('list属性设置错误' + datalistId);
 			}
 			self.init();
 		};
 
+		/**
+		 * 初始化方法
+		 */
+		this.init = function(self) {
+			self.initOptions(defaultOptions);
+			// 屏蔽浏览器默认的自动提示
+			self._set('autocomplete', 'off');
+			// 为input元素绑定事件
+			self.bindEventForInput();
+		}
+
+		/**
+		 * 定义list property，使得能够利用getter/setter设置input的list属性
+		 *
+		 * @param {String} list datalist元素的id属性
+		 */
 		this.list = property(function(self) {
 			return self.getAttribute('list');
 		}, function(self, list) {
@@ -37,17 +64,19 @@ object.add('dom.datalist', 'dom, ua, sys', function(exports, dom, ua, sys) {
 			}
 		});
 
-		this.init = function(self) {
-			self.initOptions(defaultOptions);
-			self._set('autocomplete', 'off');
-			self.bindInputEvent();
-		}
-
+		/**
+		 * 初始化options属性
+		 * @param {Object} options 属性设置
+		 */
 		this.initOptions = function(self, options) {
 			self.options = object.extend(defaultOptions, options);
 		}
 
-		this.bindInputEvent = function(self) {
+		/**
+		 * 为input绑定事件，包括focus、blur、keydown、keyup等
+		 */
+		this.bindEventForInput = function(self) {
+			// 获取焦点时显示列表
 			self.addEvent('focus', function(e) {
 				if (!self.isDisplayed() && !self.focusFromContainer) {
 					self.showDataList();
@@ -57,6 +86,7 @@ object.add('dom.datalist', 'dom, ua, sys', function(exports, dom, ua, sys) {
 				}
 			}, false);
 
+			// 焦点移除时隐藏列表
 			self.addEvent('blur', function(e) {
 				if (self.clickOnContainer) {
 					self.clickOnContainer = false;
@@ -65,6 +95,7 @@ object.add('dom.datalist', 'dom, ua, sys', function(exports, dom, ua, sys) {
 				}
 			}, false);
 
+			// 键盘按键点击时的处理
 			self.addEvent('keydown', function(e) {
 				if (!self.isDisplayed()) {
 					return;
@@ -72,23 +103,28 @@ object.add('dom.datalist', 'dom, ua, sys', function(exports, dom, ua, sys) {
 				var keyCode = e.keyCode;
 				switch (keyCode) {
 					case KEY.UP : 
+						// 向上则选择上一条
 						e.preventDefault();
 						self.previous();
 						self.getFocus();
 						break;
 					case KEY.DOWN : 
+						// 向下则选择下一条
 						e.preventDefault();
 						self.next();
 						self.getFocus();
 						break;
 					case KEY.ENTER : 
+						// 回车则选择当前项，并且隐藏列表
 						e.preventDefault();
 						if (self._li) {
 							self.value = self._li.getAttribute('real_value');
 						}
 						self.hideDataList();
+						// 屏蔽表单的默认提交
 						return false;
 					case KEY.ESC : 
+						// ESC键隐藏列表
 						self.hideDataList();
 						break;
 					default:
@@ -96,8 +132,10 @@ object.add('dom.datalist', 'dom, ua, sys', function(exports, dom, ua, sys) {
 				}
 			}, false);
 
+			// keyup的时候才能正确处理字符输入
 			self.addEvent('keyup', function(e) {
 				var keyCode = e.keyCode;
+				// TODO 判断keyCode是否是字符输入
 				switch (keyCode) {
 					case KEY.UP : 
 					case KEY.DOWN : 
@@ -112,12 +150,18 @@ object.add('dom.datalist', 'dom, ua, sys', function(exports, dom, ua, sys) {
 			}, false);
 		}
 
+		/**
+		 * 让input获取焦点
+		 */
 		this.getFocus = function(self) {
 			setTimeout(function() {
 				self.focusToPosition(self.value.length);
 			}, 0);
 		};
 
+		/**
+		 * 选择上一条记录
+		 */
 		this.previous = function(self) {
 			var scrollIndex = 0, direction = 'up', list = self._list, len = list.length, scrollIndex = len - 1;
 			if (!self._li) {
@@ -125,18 +169,21 @@ object.add('dom.datalist', 'dom, ua, sys', function(exports, dom, ua, sys) {
 			} else {
 				for(var i = 0; i < len; i++) {
 					if (list[i] === self._li) {
-						StyleUtil.remove(self._li);
+						rmStyle(self._li);
 						self._li = list[(i == 0 ? len - 1 : i - 1)];
 						scrollIndex = (i == 0 ? len - 1 : i - 1);
 						break;
 					}
 				}
 			}
-			StyleUtil.add(self._li);
+			addStyle(self._li);
 			//self.value = self._li.getAttribute('real_value');
 			self.scrollTo(scrollIndex, direction);
 		};
 
+		/**
+		 * 选择下一条记录
+		 */
 		this.next = function(self) {
 			var scrollIndex = 0, direction = 'down', list = self._list;
 			if (!self._li) {
@@ -145,19 +192,25 @@ object.add('dom.datalist', 'dom, ua, sys', function(exports, dom, ua, sys) {
 				var len = list.length;
 				for(var i = 0; i < len; i++) {
 					if (list[i] === self._li) {
-						StyleUtil.remove(self._li);
+						rmStyle(self._li);
 						self._li = list[(i == len - 1 ? 0 : i + 1)];
 						scrollIndex = (i == len - 1 ? 0 : i + 1);
 						break;
 					}
 				}
 			}
-			StyleUtil.add(self._li);
+			addStyle(self._li);
 			//self.value = self._li.getAttribute('real_value');
 			self.scrollTo(scrollIndex, direction);
 		};
 
+		/**
+		 * 控制滚动条滚动到选中的数据项上
+		 * @param {int} index 选中项的索引
+		 * @param {String} direction 按键的方向（up/down）
+		 */
 		this.scrollTo = function(self, index, direction) {
+			// TODO 干掉这一堆var
 			var scrollTop = self._ul.scrollTop;
 			var scrolled = scrollTop / self._liOffsetHeight;
 			var count = self._ul.offsetHeight / self._liOffsetHeight - 1;
@@ -183,29 +236,29 @@ object.add('dom.datalist', 'dom, ua, sys', function(exports, dom, ua, sys) {
 			}
 		};
 
-		var StyleUtil = {
-			add : function(li) {
+		function addStype(li) {
+			if (li) {
 				li.style.backgroundColor = '#316AC5';
 				li.style.color = 'white';
 				li.style.cursor = 'pointer';
-			},
-			remove : function(li) {
-				if (li) {
-					li.style.backgroundColor = 'white';
-					li.style.color = 'black';
-					li.style.cursor = 'auto';
-				}
 			}
-		};
+		}
 
-		var templates = {};
-		templates.list = 
-			'<ul style="list-style:none;margin:0;padding:0;z-index:4;position:relative;">' + 
-				'{{#data}}<li real_value="{{value}}">{{text}}</li>{{/data}}' + 
-			'</ul>';
-		//ie6下只有一条记录的时候会出现滚动条 https://github.com/brandonaaron/bgiframe/blob/master/jquery.bgiframe.js
-		templates.html = 
-			'<div id="datalistContainer" style="border:1px solid gray;position:absolute;z-index:3;left:{{left}}px;top:{{top}}px;background:#fff;font-size:small;">{{#ie6}}<iframe id="datalist_iframe" frameBorder="0" style="position:absolute;z-index:2;top:0px;left:0px;overflow:hidden;display:block;filter:Alpha(Opacity=0);" src="javascript:false;"></iframe>{{/ie6}}</div>';
+		function rmStyle(li) {
+			if (li) {
+				li.style.backgroundColor = 'white';
+				li.style.color = 'black';
+				li.style.cursor = 'auto';
+			}
+		}
+
+		var templates = {
+			list : '<ul style="list-style:none;margin:0;padding:0;z-index:4;position:relative;">' + 
+						'{{#data}}<li real_value="{{value}}">{{text}}</li>{{/data}}' + 
+				   '</ul>',
+			//ie6下只有一条记录的时候会出现滚动条 https://github.com/brandonaaron/bgiframe/blob/master/jquery.bgiframe.js
+			html : '<div id="datalistContainer" style="border:1px solid gray;position:absolute;z-index:3;left:{{left}}px;top:{{top}}px;background:#fff;font-size:small;">{{#ie6}}<iframe id="datalist_iframe" frameBorder="0" style="position:absolute;z-index:2;top:0px;left:0px;overflow:hidden;display:block;filter:Alpha(Opacity=0);" src="javascript:false;"></iframe>{{/ie6}}</div>'
+		};
 
 		this.showDataList = function(self) {
 			var data = self.getListData();
@@ -229,7 +282,7 @@ object.add('dom.datalist', 'dom, ua, sys', function(exports, dom, ua, sys) {
 					self.parentNode.appendChild(node);
 				}
 				self._container = dom.id('datalistContainer');
-				self.bindEvents();
+				self.bindEventForContainer();
 			}
 			
 			self.filter();
@@ -304,22 +357,22 @@ object.add('dom.datalist', 'dom, ua, sys', function(exports, dom, ua, sys) {
 			}
 		}
 
-		this.bindEvents = function(self) {
+		this.bindEventForContainer = function(self) {
 			var container = self._container;
 			container.delegate('li', 'mouseover', function(e) {
 				if (self._li) {
-					StyleUtil.remove(self._li);
+					rmStyle(self._li);
 				}
 				var li = e.target;
 				self._li = li;
-				StyleUtil.add(li);
+				addStyle(li);
 			});
 
 			container.delegate('li', 'mouseout', function(e) {
 				//var li = e.target;
 				//var relatedTarget = e.relatedTarget;
 				//if (relatedTarget.tagName == 'LI') {
-					StyleUtil.remove(self._li);
+					rmStyle(self._li);
 					self._li = null;
 				//}
 			});
