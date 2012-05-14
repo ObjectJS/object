ba4c7d3810b770c8ea5c93ecc15a25168b881fb1
jingwei.li <none@none>
2012-05-14 15:05:32 +0800

    nav
    --HG--
    branch : feature/ui-refactory

--- src/ui/nav.js
+++ src/ui/nav.js
@@ -8,9 +8,7 @@ this.ForeNextControl = new Class(ui.Component, function() {
 	this.nextButton = ui.define('.nextbutton');
 	this.foreButton = ui.define('.forebutton');
 
-	this.initialize = function(self, node) {
-		ui.Component.initialize(self, node);
-
+	this._init = function(self) {
 		self.loop = false; // 是否循环
 		self.total = parseInt(self._node.getData('total'));
 		self.start = parseInt(self._node.getData('start')) || 0;
@@ -33,30 +31,28 @@ this.ForeNextControl = new Class(ui.Component, function() {
 		self.fore();
 	};
 
-	this.next = events.fireevent(function(self) {
+	this._next = function(self) {
 		self.position++;
 		self.change();
-	});
+	};
 
-	this.fore = events.fireevent(function(self) {
+	this.fore = function(self) {
 		self.position--;
 		self.change();
-	});
+	};
 
-	this.change = function(self) {
-		// TODO
-		self.fireEvent('change', {forenext: self});
+	this._change = function(self) {
 		self.updateTotal();
 		self.updatePosition();
 	};
 
-	this.updatePosition = events.fireevent(function(self) {
+	this._updatePosition = function(self) {
 		self._node.getElements('.current').set('innerHTML', self.position + 1); // position是从0开始滴～展示的时候+1
-	});
+	};
 
-	this.updateTotal = events.fireevent(function(self) {
+	this._updateTotal = function(self) {
 		self._node.getElements('.total').set('innerHTML', self.total);
-	});
+	};
 
 });
 
