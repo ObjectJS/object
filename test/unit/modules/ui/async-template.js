037016f566516c9f0159b2370e02ac0df2157b7b
goto100 <goto100@gmail.com>
2013-04-25 15:29:06 +0800

    remove ui

--- test/unit/modules/ui/async-template.js
+++
@@ -1,3 +0,0 @@
-object.define('test_ui/template.mustache', function() {
-	return '{{#msg}}<h2 class="test">{{msg}}</h2>{{/msg}}'
-});
--- test/unit/modules/ui/index.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	var path = $UNIT_TEST_CONFIG.needPath ? 'modules/ui/' : '';
-	$UNIT_TEST_SCRIPT_LOADER
-	   .script(path + "ui-basic.js").wait()
-	   .script(path + "ui-addons.js").wait()
-});
-
